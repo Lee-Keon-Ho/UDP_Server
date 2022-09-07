f09eabdf32114f62b7f3efeb631c86de8f1cdd39
Lee-Keon-Ho <6150_@naver.com>
2022-09-07 15:58:34 +0900

    update

--- UDP_Server/TcpListener.h
+++ UDP_Server/TcpListener.h
@@ -1,20 +1,19 @@
-//#pragma once
-//#include <winsock2.h>
-//
-//class CTcpListener
-//{
-//protected:
-//	SOCKET m_listenSocket;
-//	SOCKET m_clientSocket;
-//	sockaddr_in m_addr;
-//	sockaddr_in m_addrClient;
-//
-//public:
-//	CTcpListener();
-//	~CTcpListener();
-//
-//	bool Init(PCSTR _ip, u_short _port);
-//
-//	bool Loop();
-//	bool OnAccept();
-//};
+#pragma once
+#include <WinSock2.h>
+
+class CTcpListener
+{
+protected:
+	SOCKET m_listenSocket;
+	SOCKET m_clientSocket;
+	sockaddr_in m_addr;
+	sockaddr_in m_addrClient;
+
+public:
+	CTcpListener();
+	~CTcpListener();
+
+	bool Init(PCSTR _ip, u_short _port);
+
+	bool onAccept();
+};
---
+++ UDP_Server/UserTcpListener.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "TcpListener.h"
+
+class CUserTcpListener : public CTcpListener
+{
+private:
+
+public:
+	CUserTcpListener();
+	~CUserTcpListener();
+};
